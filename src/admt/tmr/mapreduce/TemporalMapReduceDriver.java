bac5b1e88063ee57f549a55cc1055c93bcf22a93
luca91 <lucabelles@gmail.com>
2014-12-28 15:52:02 +0100

    project files added

---
+++ src/admt/tmr/mapreduce/TemporalMapReduceDriver.java
@@ -0,0 +1,21 @@
+package admt.tmr.mapreduce;
+
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+public class TemporalMapReduceDriver extends Configured implements Tool {
+
+	@Override
+	public int run(String[] arg0) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
